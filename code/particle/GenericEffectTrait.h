369c70d21da9ef258e08e34a5d29ed36a66209e4
asarium <asarium@gmail.com>
2013-06-04 10:12:25 +0200

    Moved TraitUtil namespace into own file
    Also moved some general utility functions from GenericEffectTrait into
    this namespace.

--- code/particle/GenericEffectTrait.h
+++ code/particle/GenericEffectTrait.h
@@ -6,21 +6,9 @@
 #include "object/object.h"
 #include "particle/EffectTrait.h"
 #include "particle/particle.h"
+#include "particle/TraitUtil.h"
 
 #include <boost/smart_ptr.hpp>
-
-template<class ValueType>
-struct ValueRange
-{
-	ValueType upper;
-	ValueType lower;
-
-	ValueRange() {}
-
-	ValueRange(const ValueType& value) : upper(value), lower(value) {}
-
-	ValueRange(const ValueType& value1, const ValueType& value2) : upper(value1), lower(value2) {}
-};
 
 struct GenericTraitConfiguration
 {
@@ -35,10 +23,11 @@ struct GenericTraitConfiguration
 	ValueRange<float> particleVelocity;
 
 	bool addObjectVelocity;
+	bool reflectDirection;
 
 	GenericTraitConfiguration() : trailEffectID(-1), effectAnimation(-1), addObjectVelocity(true),
 		particleLifetime(ValueRange<float>(-1.0f)), particleNumber(ValueRange<int>(1)), beginDelay(ValueRange<int>(0)),
-		particleVelocity(ValueRange<float>(0.0f))
+		particleVelocity(ValueRange<float>(0.0f)), reflectDirection(false)
 	{}
 };
 
@@ -54,10 +43,6 @@ protected:
 
 	bool spawningParticles(const ParticleSource& source);
 
-	vec3d getEffectPosition(const ParticleSource& source, bool* success = NULL) const;
-
-	vec3d getEffectDirection(const ParticleSource& source, bool* success = NULL, bool addVelocity = true) const;
-
 	boost::weak_ptr<particle> createParticle(vec3d* position, vec3d* vel, int animation, float lifetime, float radius, object* obj = NULL);
 public:
 	GenericEffectTrait(const SCP_string& name);
@@ -71,21 +56,6 @@ public:
 	virtual bool isActive(const ParticleSource& source) const;
 
 	virtual void doParse();
-};
-
-namespace TraitUtil
-{
-	template<class ValueType>
-	ValueType randomRange(const ValueType lower, const ValueType upper);
-
-	template<class ValueType>
-	inline ValueType randomRange(const ValueRange<ValueType>& range)
-	{
-		return randomRange(range.lower, range.upper);
-	}
-	
-	template<class ValueType>
-	ValueRange<ValueType> parseValueRange(const ValueType lowerBound, const ValueType upperBound);
 };
 
 #endif // _GENERIC_EFFECT_TRAIT_H
---
+++ code/particle/TraitUtil.h
@@ -0,0 +1,42 @@
+
+#ifndef _TRAIT_UTIL_H
+#define _TRAIT_UTIL_H
+
+#include "globalincs/pstypes.h"
+#include "particle/ParticleSource.h"
+
+template<class ValueType>
+struct ValueRange
+{
+	ValueType upper;
+	ValueType lower;
+
+	ValueRange() {}
+
+	ValueRange(const ValueType& value) : upper(value), lower(value) {}
+
+	ValueRange(const ValueType& value1, const ValueType& value2) : upper(value1), lower(value2) {}
+};
+
+namespace TraitUtil
+{
+	template<class ValueType>
+	ValueType randomRange(const ValueType lower, const ValueType upper);
+
+	template<class ValueType>
+	inline ValueType randomRange(const ValueRange<ValueType>& range)
+	{
+		return randomRange(range.lower, range.upper);
+	}
+	
+	template<class ValueType>
+	ValueRange<ValueType> parseValueRange(const ValueType lowerBound, const ValueType upperBound);
+
+	vec3d getEffectPosition(const ParticleSource& source, bool* success = NULL);
+
+	vec3d getEffectDirection(const ParticleSource& source, bool* success = NULL, bool addVelocity = true);
+
+	vec3d getReflectionVector(vec3d* incoming, vec3d* normal);
+};
+
+#endif
