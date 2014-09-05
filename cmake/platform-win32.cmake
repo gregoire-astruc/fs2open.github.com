c2359cd764f7029bd903c45d244cedd5503b0a2d
asarium <asarium@gmail.com>
2014-09-05 10:14:48 +0200

    Use CMake to link against the OpenGL libraries

--- cmake/platform-win32.cmake
+++ cmake/platform-win32.cmake
@@ -3,11 +3,13 @@ MESSAGE(STATUS "Configuring Windows specific things and stuff...")
 
 SET(PLATFORM_INCLUDES "" CACHE INTERNAL "Compiler specific includes")
 SET(PLATFORM_LBRARIES "" CACHE INTERNAL "Compiler specific libraries")
-	
+
 # DirectX libraries are still used on windows
 SET(DIRECTX_DIRECTORY ${CMAKE_SOURCE_DIR}/code/directx)
 
-SET(PLATFORM_LBRARIES 
+FIND_PACKAGE(OpenGL REQUIRED)
+
+SET(PLATFORM_LBRARIES
 	comctl32.lib
 	msacm32.lib
 	odbc32.lib
@@ -17,7 +19,8 @@ SET(PLATFORM_LBRARIES
 	wsock32.lib
 	${DIRECTX_DIRECTORY}/dxguid.lib
 	${DIRECTX_DIRECTORY}/strmiids.lib
-	${DIRECTX_DIRECTORY}/vdinput.lib)
+	${DIRECTX_DIRECTORY}/vdinput.lib
+	${OPENGL_LIBRARIES})
 
 
 ADD_DEFINITIONS(-DWIN32)
