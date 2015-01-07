717042d90db1b2389dd20444cc1de7b540342b9c
Grégoire Astruc <gregoire.astruc@gmail.com>
2015-01-07 10:07:47 +0100

    Use apple openal includes.

--- code/sound/openal.h
+++ code/sound/openal.h
@@ -6,6 +6,9 @@
 #if !(defined(__APPLE__) || defined(_WIN32))
 	#include <AL/al.h>
 	#include <AL/alc.h>
+#elif defined(__APPLE__)
+	#include <OpenAL/al.h>
+	#include <OpenAL/alc.h>
 #else
 	#include "al.h"
 	#include "alc.h"
